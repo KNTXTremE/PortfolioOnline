754373943e41028ab88b5950ffd8582690be76f5
KNTXTremE <kan.horwang@gmail.com>
2019-07-13 19:27:24 +0700

    Add Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume-online';
+  a = 'your name'
+
+  constructor(){
+    this.changeA();
+  }
+
+  changeA(){
+    var i: number;
+    i = 2;
+    if(i == 1) this.a = 'your name'
+    else this.a = 'not'
+  }
+  
 }

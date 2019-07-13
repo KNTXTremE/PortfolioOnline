11c4c81dd693b547a963c6f685b86d4eb04b8fc6
KNTXTremE <kan.horwang@gmail.com>
2019-07-13 20:34:17 +0700

    Add features
    - Auto redirect path / to /home in app routing module
    - add about me component and routerlink
    - partially fix responsive collapse navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about-me', component: AboutMeComponent}
 ];
 
 @NgModule({
